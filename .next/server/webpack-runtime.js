85707101ad6d986cc424c00f6394b70f1e8c4dea
Mishawn Lolis <mjjllolis@icloud.com>
2025-08-27 09:33:41 -0500

    Mobile first adjustments

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,6 +47,11 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	(() => {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -125,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("ac87d02f18ef2a0f")
+/******/ 		__webpack_require__.h = () => ("27bc89cd2354cf74")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f5c066e435cae133"; }
+/******/ 		__webpack_require__.h = function() { return "f58778b3c5bc28f1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
